a5f8e33686081d448c4a281f6b94d8366019d977
saps999 <saptarshibiswasgang@gmail.com>
2022-10-07 13:52:28 +0530

    Online store system

--- src/com/bridgelabz/StoreMain.java
+++ src/com/bridgelabz/StoreMain.java
@@ -29,14 +29,14 @@ public class StoreMain {
      */
         store.printProductList();
     /*
-    taking input from customer for choosing the product and adding into cart
+    taking input from customer for selecting the menu option
      */
-        //System.out.println("Enter 1 for Maggie, 2 for Dairy Milk, 3 for Lay's, 4 for Kit-Kat, and 5 for KurKure to add the product in cart: ");
         Scanner sc = new Scanner(System.in);
         int choice = 0;
         boolean keepGoing = true;
         while (keepGoing) {
-            System.out.println("\nMenu - Managing a List");
+
+            System.out.println("\n-------------------Menu - Managing a List-------------------");
             System.out.println("1 Add Maggie to your cart");
             System.out.println("2  Add Dairy Milk to your cart");
             System.out.println("3 Add Lay's to your cart");
@@ -45,36 +45,39 @@ public class StoreMain {
             System.out.println("6 Show cart");
             System.out.println("7 Exit");
             System.out.println("Select a menu option");
+
             choice = sc.nextInt();
+
             if (choice < 1 || choice > 7) {
                 System.out.println("Enter a value between 1 and 7:");
             } else {
-                switch (choice) {
-                    case 1:
-                        //add an integer
-                        store.addToCart(product1);
 
-                        //intList.add(input);
+                switch (choice) {
+
+                    case 1:
+                        store.addToCart(product1);
                         break;
+
                     case 2:
-                        //remove from the list
                         store.addToCart(product2);
                         break;
+
                     case 3:
-                        //view the items in your cart
                         store.addToCart(product3);
                         break;
+
                     case 4:
-                        //Exit and add up the total
                         store.addToCart(product4);
                         break;
+
                     case 5:
-                        //Empty the list
                         store.addToCart(product5);
                         break;
+
                     case 6:
                         store.printCartList();
                         break;
+
                     case 7:
                         //exit
                         keepGoing = false;
@@ -84,29 +87,6 @@ public class StoreMain {
                 }
             }
         }
-
-            /*if (choice==2) {
-                break;
-            }else {
-                System.out.println("Enter 1 for Maggie, 2 for Dairy Milk, 3 for Lay's, 4 for Kit-Kat, and 5 for KurKure to add the product in cart: ");
-                if (a <= 5 && a >= 1) {
-
-                    if (a == 1) {
-                        store.addToCart(product1);
-                    } else if (a == 2) {
-                        store.addToCart(product2);
-                    } else if (a == 3) {
-                        store.addToCart(product3);
-                    } else if (a == 4) {
-                        store.addToCart(product4);
-                    } else if (a == 5) {
-                        store.addToCart(product5);
-                    }
-                } else System.out.println("Choose the correct option.");
-            }
-        }*/
-
-
     }
 }
 
